157b0c61bff98862729e46ea5f80e9b965f61048
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-03 14:38:52 -0600

    updated while

--- forms/while/while.js
+++ forms/while/while.js
@@ -1,5 +1,10 @@
 let num = 1
+<<<<<<< Updated upstream
 while (num<=10) {
+=======
+let num2 = prompt('How many times do you wamt this loop to run?')
+while (num<=num2) {
+>>>>>>> Stashed changes
   console.log(`This is loop iteration ${num}.`);
   num++;
   }
